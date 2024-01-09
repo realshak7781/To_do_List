1e73d53e26dcd7a9ff7a1be3647d0df9acfd2474
@fit_Monk <124501803+realshak7781@users.noreply.github.com>
2024-01-09 16:03:30 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,46 @@
+var inputBox=document.getElementById("input-box");
+var listContainer=document.getElementById("list-container");
+
+
+function showTask(){
+    listContainer.innerHTML=localStorage.getItem("data");
+}
+
+showTask();
+
+function addTask(){
+    if(inputBox.value===""){
+        alert("Kuch kaam  add kr pehle!");
+    }
+    else{
+        let li=document.createElement("li");
+        li.innerHTML=inputBox.value;
+        listContainer.appendChild(li);
+        let span=document.createElement("span");
+        span.innerHTML="\u00d7";
+        li.appendChild(span);
+    }
+    inputBox.value="";
+    saveData();
+}
+
+listContainer.addEventListener("click", function(e) {
+    if (e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");
+        saveData();
+
+    } else if (e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+        saveData();
+    }
+});
+
+
+function saveData(){
+    localStorage.setItem("data",listContainer.innerHTML);
+}
+
+function showTask(){
+    listContainer.innerHTML=localStorage.getItem("data");
+}
+
